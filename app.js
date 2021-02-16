c67881293b79660770ce75d04cebd6f18415e209
Helder Balbino <helderbalbino@googlemail.com>
2021-02-16 12:08:49 +0000

    Added further js logic to the app

--- app.js
+++ app.js
@@ -6,10 +6,25 @@ function addNewEntry(newEntry) {
 	entriesWrapper.removeChild(entriesWrapper.firstElementChild);
 
 	const listItem = document.createElement('li');
-	const listValue = document.createTextNode(newEntry);
+	const listValue = document.createTextNode(newEntry.toFixed(1));
 	listItem.appendChild(listValue);
 
 	entriesWrapper.appendChild(listItem);
+}
+
+function reducer(total, currentValue) {
+	return total + currentValue;
+}
+
+function calculateTotal(userEntries) {
+	const totalValue = userEntries.reduce(reducer).toFixed(1);
+	document.getElementById('total').innerText = totalValue;
+	document.getElementById('progressTotal').innerText = totalValue;
+}
+
+function calculateAverage() {
+	const average = (userEntries.reduce(reducer) / userEntries.length).toFixed(1);
+	document.getElementById('average').innerText = average;
 }
 
 function handleSubmit(event) {
@@ -21,6 +36,8 @@ function handleSubmit(event) {
 	document.querySelector('form').reset();
 	userEntries.push(entry);
 	addNewEntry(entry);
+	calculateTotal(userEntries);
+	calculateAverage(userEntries);
 }
 
 const form = document.querySelector('form').addEventListener('submit', handleSubmit);
