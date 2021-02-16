2c9b75e7c7fbddd2a51eda9c8e5d9d3a14d44360
Helder Balbino <helderbalbino@googlemail.com>
2021-02-16 11:23:47 +0000

    Even further styling and added some logic with js

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
+let userEntries = [];
+
+const entriesWrapper = document.querySelector('#entries');
+
+function addNewEntry(newEntry) {
+	entriesWrapper.removeChild(entriesWrapper.firstElementChild);
+
+	const listItem = document.createElement('li');
+	const listValue = document.createTextNode(newEntry);
+	listItem.appendChild(listValue);
+
+	entriesWrapper.appendChild(listItem);
+}
+
 function handleSubmit(event) {
 	event.preventDefault();
 	const entry = Number(document.querySelector('#numberInput').value);
-	console.log(entry);
+	if (!entry) {
+		return;
+	}
+	document.querySelector('form').reset();
+	userEntries.push(entry);
+	addNewEntry(entry);
 }
 
 const form = document.querySelector('form').addEventListener('submit', handleSubmit);
